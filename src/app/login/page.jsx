9d31b78da027c2734444d327fb93a46d3d7a2386
Guillermo <guillermogcasey@gmail.com>
2024-06-02 20:05:40 -0300

    starting to mock up the application. working on nav bar

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/layout/navbar/navbar';
+import Login from '@/components/ui/login';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <Login></Login>
+        </>
+    );
+}
+
+export default Page;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,10 @@
+import Register from '@/components/ui/register/register';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <Register></Register>
+    );
+}
+
+export default Page;
