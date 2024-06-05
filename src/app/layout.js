349431cffccc1b7e35559195e40319f0b8689ec7
Guillermo <guillermogcasey@gmail.com>
2024-06-04 23:31:35 -0300

    working on login

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SessionAuthProvider from "@/components/context/SessionAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+    <SessionAuthProvider>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </SessionAuthProvider>
     </html>
   );
 }
--- src/components/ui/login/login.jsx
+++ src/components/ui/login/login.jsx
@@ -1,6 +1,128 @@
-import React from 'react';
+'use client';
+import css from './login.css';
+import { useRef, useState, useEffect } from 'react';
+import { signIn } from 'next-auth/react';
+import Link from 'next/link';
+import LoginButton from './loginButton';
 
 const Login = () => {
+    const [loading, setLoading] = useState(false);
+    const [emailError, setEmailError] = useState({ initial: true, message: null });
+    const [passwError, setPasswError] = useState({ initial: true, message: null });
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const refs = {
+        emailRef: useRef(),
+        passRef: useRef(),
+        msgRef: useRef(),
+    }
+
+    useEffect(() => {
+        if (emailError.initial || passwError.initial || emailError.message || passwError.message) {
+            setIsDisabled(true);
+        } else {
+            setIsDisabled(false);
+        }
+    }, [emailError, passwError]);
+
+    function printStuff(){
+        console.log("loading: " + loading);
+        console.log("isDisabled: " + isDisabled);
+    }
+
+    async function handleLoginSubmit(){
+        const email = refs.emailRef.current.value;
+        const password = refs.passRef.current.value;
+
+        try {
+            setLoading(true);
+            const responseNextAuth = await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+            });
+
+            if (responseNextAuth.ok) {
+                console.log(responseNextAuth);
+                //sessionStorage.settItem("sessionUser", responseNextAuth)
+                // Handle if it's right
+            }else{
+                if(responseNextAuth.error === 'fetch failed'){
+                    showErrorMessage('El servidor no envió una respuesta.')
+                }else{
+                    showErrorMessage(responseNextAuth.error);
+                }
+            }
+        } catch (error) {
+            showErrorMessage('Ocurrió un error en el servidor');
+        }finally{
+            setLoading(false);
+        }
+
+    }
+
+    function showErrorMessage(mensaje, ms){
+        if (!ms) ms = 5000;
+        setErrorMessage(mensaje);
+        setTimeout(() => {
+            setErrorMessage('');
+        }, ms);
+    }
+
+    let emailInputValue = '';
+    let timeoutId;
+
+    const handleEmailChange = e => {
+        clearTimeout(timeoutId);
+        emailInputValue = e.target.value;
+        timeoutId = setTimeout(() => {
+            validateEmail(e.target.value);
+        }, 300);
+    }
+    const validateEmail = email => {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(emailPattern.test(email)){
+            setEmailError({
+                initial: false,
+                message: null
+            })
+            refs.emailRef.current.classList.remove('outline', 'outline-2', 'outline-rose-500');
+        }else{
+            setEmailError({
+                initial: false,
+                message: 'Email no válido'
+            })
+            refs.emailRef.current.classList.add('outline', 'outline-2', 'outline-rose-500');
+        }
+    }
+
+    let passwInputValue = '';
+
+    const handlePassChange = e => {
+        clearTimeout(timeoutId);
+        passwInputValue = e.target.value;
+        timeoutId = setTimeout(() => {
+            validatePassw(e.target.value);
+        }, 600);
+    }
+
+    const validatePassw = passw => {
+        if(passw.length >= 4){
+            setPasswError({
+                initial: false,
+                message: null
+            })
+            refs.passRef.current.classList.remove('outline-2', 'outline', 'outline-rose-500');
+        }else{
+            setPasswError({
+                initial: false,
+                message: 'Contraseña no válida'
+            })
+            refs.passRef.current.classList.add('outline', 'outline-2', 'outline-rose-500');
+        }
+    }
+
     return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -9,34 +131,39 @@ const Login = () => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-            <div>
-                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Correo electrónico</label>
-                <div className="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            <form className="space-y-6" action="#" method='post'>
+                {/* Email */}
+                <div>
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Correo electrónico</label>
+                    <div className="mt-1 c-input-container">
+                        <input id="email" name="email" type="email" autoComplete="email" onChange={handleEmailChange} ref={refs.emailRef} required className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        { emailError.message && <span className='text-xs c-input-text text-red-600 bg-white'>{emailError.message}</span>}
+                    </div>
                 </div>
-            </div>
-
-            <div>
-                <div className="flex items-center justify-between">
-                <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
-                <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Olvidaste tu clave?</a>
+                {/* Contraseña */}
+                <div> 
+                    <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                    <div className="text-sm">
+                        <Link href={'/cambiarclave'} className="font-semibold text-indigo-600 hover:text-indigo-500">Olvidaste tu clave?</Link>
+                    </div>
+                    </div>
+                    <div className="mt-1 c-input-container">
+                        <input id="password" name="password" type="password" onChange={handlePassChange} ref={refs.passRef} autoComplete="current-password" required className="box-border block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        {passwError.message && <span className='text-xs c-input-text text-red-600 bg-white'>{passwError.message}</span>}
+                    </div>
                 </div>
+                <div className='min-h-5'>
+                    <span ref={refs.msgRef} className='text-sm text-red-600 bg-white'>{errorMessage}</span>
                 </div>
-                <div className="mt-2">
-                <input id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                {/* Boton */}
+                <div>
+                    <LoginButton loading={loading} enabled={isDisabled} handleLoginSubmit={handleLoginSubmit}></LoginButton>
                 </div>
-            </div>
-
-            <div>
-                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Iniciar sesión</button>
-            </div>
             </form>
 
-            <p className="mt-10 text-center text-sm text-gray-500">
-            Todavía no te uniste?
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Crear una cuenta</a>
+            <p className="mt-5 text-center text-sm text-gray-500"> Todavía no te uniste?
+                <Link href={'/crearCuenta'} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Crear una cuenta</Link>
             </p>
         </div>
     </div>
